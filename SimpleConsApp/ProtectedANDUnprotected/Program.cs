347f448d1d17289c5b0f76604c3b92698d15358e
VanHakobyan <van19962013@mail.ru>
2017-05-06 19:21:25 +0400

    add Unprotect Section:ok:

---
+++ SimpleConsApp/ProtectedANDUnprotected/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+namespace ProtectedANDUnprotected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings("COnnectionStr1", "SomeConnectionString"));
+            config.Save();
+
+            ConnectionStringsSection section = config.GetSection("connectionStrings") as ConnectionStringsSection;
+            if (section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.UnprotectSection();
+            }
+            else
+            {
+                section.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+            }
+            config.Save();
+            Console.WriteLine($"Protected: {section.SectionInformation.IsProtected}");
+            Console.WriteLine();
+        }
+    }
+}
