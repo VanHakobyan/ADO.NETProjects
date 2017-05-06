c002aba498d8ddac3d32e2e5b052e7913eaeae72
VanHakobyan <van19962013@mail.ru>
2017-05-06 15:43:17 +0400

    add :v: WPF app connection login and password to shopdb :ok:

---
+++ SimpleConsApp/LoginPasswordSQL/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginPasswordSQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Log_Click(object sender, RoutedEventArgs e)
+        {
+            string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShopDB;" +
+                "User ID=" + LoginT.Text + ";" +
+                "Password=" + PasswordT.Text + ";";
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Connection " + connection.State + " to " + connection.Database);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
