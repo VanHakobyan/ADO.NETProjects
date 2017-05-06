b6ea54d64fa396a14ac08e7e7b91e501be6e8fce
VanHakobyan <van19962013@mail.ru>
2017-05-06 18:49:40 +0400

    :v:  Connection String Settings manipulation :ok:

---
+++ SimpleConsApp/ConfigString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace ConfigString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionStringSettings setting = new ConnectionStringSettings
+            {
+                Name = "MySetting",
+                ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Ini;Initial Catalog=ShopDB;Integrated Security=True;"
+
+            };
+            Configuration config;
+            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Add(setting);
+            config.Save();
+            Console.WriteLine("Saving Completed!!!");
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["MySetting"].ConnectionString);
+        }
+    }
+}
