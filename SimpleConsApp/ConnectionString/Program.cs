5bba1dc70edf539ff06babe7d1c9417f7e4503e1
VanHakobyan <van19962013@mail.ru>
2017-05-06 14:39:05 +0400

    add simple connection string example :+1:

---
+++ SimpleConsApp/ConnectionString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShopDB; Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connStr);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("connection opened");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("connection closed");
+            }
+        }
+    }
+}
