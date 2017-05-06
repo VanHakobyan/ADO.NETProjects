bc73efb1e5579b98293b14981cdfd530e9cb960f
VanHakobyan <van19962013@mail.ru>
2017-05-06 18:30:24 +0400

    :+1: add example pooling

---
+++ SimpleConsApp/Pooling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder conSB = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB",
+                IntegratedSecurity = true,
+                InitialCatalog = "ShopDB",
+                Pooling = true    //comment
+                //Pooling = false //uncomment
+            };
+
+            DateTime start = DateTime.Now;
+            for (int i = 0; i < 1000; i++)
+            {
+                SqlConnection connection = new SqlConnection(conSB.ConnectionString);
+                connection.Open();
+                connection.Close();
+            }
+            TimeSpan time = DateTime.Now - start;
+            Console.WriteLine(time.TotalSeconds);
+        }
+    }
+}
