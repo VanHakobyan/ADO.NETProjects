f0ebef245835468397636427a8e5af2180fc40c0
VanHakobyan <van19962013@mail.ru>
2017-05-09 15:51:36 +0400

    add :eyes:execute table :point_up:

---
+++ Commands/ExecuteTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShopDB;Integrated Security=True;";
+            SqlConnection connection = new SqlConnection(conString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Customers", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.WriteLine(reader.GetName(i)+": "+reader[i]);
+                }
+                Console.WriteLine(new string('-',20));
+            }
+            reader.Close();
+            connection.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
