d66f03346809cf8d672711d77de372debea282b5
VanHakobyan <van19962013@mail.ru>
2017-05-09 17:28:25 +0400

    add async and non sync method execute :five:

---
+++ Commands/ExecuteAsync/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+
+namespace ExecuteAsync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string conString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShopDB;Integrated Security=True;";
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection= new SqlConnection(conString))
+            {
+                connection.Open();
+                using (SqlCommand cmd= new SqlCommand("WAITFOR DELAY '00:00:07'",connection))
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Execute non async");
+                }
+            }
+        }
+
+        private async void button1_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+              await  connection.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("WAITFOR DELAY '00:00:07'", connection))
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                    MessageBox.Show("Execute async");
+                }
+            }
+        }
+    }
+}
