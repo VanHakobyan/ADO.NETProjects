28ed96bee30e32f4afbefdd13ce82765ff899c2a
VanHakobyan <van19962013@mail.ru>
2017-05-09 12:50:44 +0400

    add firs method :one:

---
+++ Commands/Command/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder conStr = new SqlConnectionStringBuilder();
+            conStr.DataSource = @"(localdb)\MSSQLLocalDB";
+            conStr.InitialCatalog = "ShopDB";
+            conStr.IntegratedSecurity = true;
+            SqlConnection connection = new SqlConnection(conStr.ConnectionString);
+            connection.Open();
+            //First method----------------------
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "select * from ShopDB";
+            connection.Close();
+        }
+    }
+}
