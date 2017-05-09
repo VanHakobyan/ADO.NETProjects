c6d5a10668dfae04574f1d4dda8b1e0332f300dd
VanHakobyan <van19962013@mail.ru>
2017-05-09 13:44:49 +0400

    add scalar executing :v:

---
+++ Commands/ExecuteCommand/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder conStr = new SqlConnectionStringBuilder();
+            conStr.DataSource = @"(localdb)\MSSQLLocalDB";
+            conStr.InitialCatalog = "ShopDB";
+            conStr.IntegratedSecurity = true;
+            SqlConnection connection = new SqlConnection(conStr.ConnectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT Phone FROM Customers where CustomerNo=3",connection);
+            string phone = (string)cmd.ExecuteScalar();
+            Console.WriteLine(phone);      
+            connection.Close();
+        }
+    }
+}
