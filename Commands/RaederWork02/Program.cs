1be37c8d0df33fee6beb753113609cd1f44cea8c
VanHakobyan <van19962013@mail.ru>
2017-05-09 16:13:46 +0400

    add :snowflake: Reader02

---
+++ Commands/RaederWork02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaederWork02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShopDB;Integrated Security=True;";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Customers", connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0]);
+                        Console.WriteLine(reader["FName"] + " " + reader["LName"]);
+                        Console.WriteLine(reader[7]);
+                    }
+                }
+            }
+
+        }
+    }
+}
