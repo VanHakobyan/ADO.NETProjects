060f4c25998023df6fcc6986fad8cf05c666c99a
VanHakobyan <van19962013@mail.ru>
2017-05-11 17:16:01 +0400

    add 2:dash: project to :dash:dirty reading:dash:

---
+++ DirtyRead/DirtyReadfirst/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DirtyReadfirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=(Localdb)\MSSQLLocalDB; Initial Catalog=ShopDB; Integrated Security=True;"; // создание строки подключения
+
+            SqlConnection connection = new SqlConnection(conStr);
+
+            SqlCommand cmd = new SqlCommand("UPDATE Customers SET Phone = 'TEST' WHERE CustomerNo = 1", connection);
+            //cmd = new SqlCommand("UPDATE Customers SET Phone = '(052)1245789' WHERE CustomerNo = 1", connection);
+
+            Console.WriteLine("Step 1. Press any key to execute command...");
+            Console.ReadKey();
+
+            connection.Open();
+
+            cmd.Transaction = connection.BeginTransaction();
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Step 3. Press any key to rollback transaction...");
+            Console.ReadKey();
+
+            cmd.Transaction.Rollback();
+            connection.Close();
+
+            Console.WriteLine("Transaction rollback");
+
+
+        }
+    }
+}
---
+++ DirtyRead/DirtyReadsecond/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DirtyReadsecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=(Localdb)\MSSQLLocalDB; Initial Catalog=ShopDB;  Integrated Security=True;"; // создание строки подключения
+
+            SqlConnection connection = new SqlConnection(conStr);
+
+            SqlCommand cmd = new SqlCommand("SELECT LName, FName, Phone FROM Customers", connection);
+
+            Console.WriteLine("Step 2. Press any key to read Customers");
+            Console.ReadKey();
+
+            connection.Open();
+            cmd.Transaction = connection.BeginTransaction(IsolationLevel.ReadUncommitted);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1}: {2}", reader[0], reader[1], reader[2]);
+            }
+
+            connection.Close();
+        }
+    }
+}
