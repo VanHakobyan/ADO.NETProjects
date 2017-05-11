863fed8e3d817a49f9ac63765c987031aedc4931
VanHakobyan <van19962013@mail.ru>
2017-05-11 17:50:54 +0400

    add  Parameters.AddWithValue :ok:

--- ParametrizedCommands/ParametrizedC1/Program.cs
+++ ParametrizedCommands/ParametrizedC1/Program.cs
@@ -13,15 +13,16 @@ namespace ParametrizedC1
         {
             Console.WriteLine("Input number:");
             string ID = Console.ReadLine();
-            string sqlComm = string.Format($"Select * from customers where CustomerNo={ID}");
+            string sqlComm = string.Format($"Select * from customers where CustomerNo=@No");
             SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
             sb.DataSource = "(localdb)\\MSSQLLocalDB";
             sb.InitialCatalog = "ShopDB";
             sb.IntegratedSecurity = true;
             using (SqlConnection connection = new SqlConnection(sb.ConnectionString))
             {
-                connection.Open();
                 SqlCommand cmd = new SqlCommand(sqlComm, connection);
+                cmd.Parameters.AddWithValue("No", ID);
+                connection.Open();
                 using (SqlDataReader reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
