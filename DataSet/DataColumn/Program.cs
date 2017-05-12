3cced09312b5592b1a0c7a9da9f93237be3e2e42
VanHakobyan <van19962013@mail.ru>
2017-05-12 15:22:38 +0400

    add DataSet and new project DataColumn :one:

---
+++ DataSet/DataColumn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace DataColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable newTable = new DataTable("First");
+            DataColumn first = new DataColumn("name", typeof(string));
+            DataColumn second = new DataColumn("age", typeof(int));
+            DataColumnCollection coll = newTable.Columns;
+            coll.AddRange(new DataColumn[] { first, second });
+            foreach (DataColumn item in newTable.Columns)
+            {
+                Console.WriteLine(item.ColumnName+" "+item.DataType);
+                Console.WriteLine(new string('-',40));
+            }
+
+        }
+    }
+}
---
+++ ParametrizedCommands/Parameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+            sb.DataSource = "(localdb)\\MSSQLLocalDB";
+            sb.InitialCatalog = "ShopDB";
+            sb.IntegratedSecurity = true;
+            SqlConnection connection = new SqlConnection(sb.ConnectionString);
+            SqlCommand cmd = new SqlCommand("SET @Parameter=2;", connection);
+            SqlParameter param = cmd.Parameters.Add("Parameter", System.Data.SqlDbType.Int);
+            param.Direction = System.Data.ParameterDirection.Output;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Value:"+param.Value);
+            Console.ReadKey();
+            connection.Close();
+
+
+        }
+    }
+}
