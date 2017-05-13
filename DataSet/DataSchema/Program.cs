4a19d6539fbee67635e533de6d7463fac6309b06
VanHakobyan <van19962013@mail.ru>
2017-05-13 23:14:56 +0400

    add Schema and work :v:

---
+++ DataSet/DataSchema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\ADO.NET\DATA\ShopDB.mdf; Integrated Security = True; Connect Timeout = 30");
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("select * from customers", connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable schema = reader.GetSchemaTable();
+            foreach (DataRow row in schema.Rows)
+            {
+                foreach (DataColumn col in schema.Columns)
+                {
+                    Console.WriteLine($"{col.ColumnName} {row[col]}");
+                    Console.WriteLine();
+                }
+            }
+
+          //  DataTable customers = new DataTable("customers");
+
+        }
+    }
+}
