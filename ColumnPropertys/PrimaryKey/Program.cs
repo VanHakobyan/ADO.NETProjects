2f1be3b21498fc36407dc751f07d90698a390d19
VanHakobyan <van19962013@mail.ru>
2017-05-14 15:35:41 +0400

    add primary key unique :v:

---
+++ ColumnPropertys/PrimaryKey/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaryKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+            DataColumn col1 = table.Columns.Add("first");
+            DataColumn col2 = table.Columns.Add("second");
+
+            table.Constraints.Add(new UniqueConstraint(col1, true));
+            Console.WriteLine(table.Columns[0].Unique);
+            Console.WriteLine(table.PrimaryKey.Length);
+            Console.WriteLine(table.PrimaryKey[0].ColumnName);
+        }
+    }
+}
